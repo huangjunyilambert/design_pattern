321a1a96aa24b1c5db388ecec2420657c7c873df
huangjunyi <huangjy@tuya.com>
2022-09-22 16:46:12 +0800

    享元模式

---
+++ src/design/patterns/flyweight/Shape.java
@@ -0,0 +1,9 @@
+package design.patterns.flyweight;
+
+/**
+ * @author huangjunyi
+ * @date 2022/9/22 16:41:12
+ */
+public interface Shape {
+    void draw();
+}
---
+++ src/design/patterns/flyweight/ShapeFactory.java
@@ -0,0 +1,22 @@
+package design.patterns.flyweight;
+
+import java.util.HashMap;
+
+/**
+ * @author huangjunyi
+ * @date 2022/9/22 16:42:09
+ */
+public class ShapeFactory {
+    private static final HashMap<String, Shape> circleMap = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) circleMap.get(color);
+
+        if (circle == null) {
+            circle = new Circle(color);
+            circleMap.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
