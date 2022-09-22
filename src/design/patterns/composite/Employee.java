301193c9926fc5f1a67a37ecc140d7db71e42655
huangjunyi <huangjy@tuya.com>
2022-09-22 14:33:41 +0800

    组合模式

---
+++ src/design/patterns/composite/Employee.java
@@ -0,0 +1,41 @@
+package design.patterns.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author huangjunyi
+ * @date 2022/9/22 14:30:07
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name
+                + ", dept : " + dept + ", salary :"
+                + salary + " ]");
+    }
+}
