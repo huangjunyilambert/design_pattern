b6a971442436be7695472f01ff23870e2f783226
huangjunyi <huangjy@tuya.com>
2022-09-29 15:25:25 +0800

    传输对象模式

---
+++ src/design/extend/transferobject/StudentVO.java
@@ -0,0 +1,31 @@
+package design.extend.transferobject;
+
+/**
+ * @author huangjunyi
+ * @date 2022/9/29 15:18:59
+ */
+public class StudentVO {
+    private String name;
+    private int rollNo;
+
+    StudentVO(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+}
---
+++ src/design/extend/transferobject/TransferObjectPatternDemo.java
@@ -0,0 +1,27 @@
+package design.extend.transferobject;
+
+/**
+ * @author huangjunyi
+ * @date 2022/9/29 15:20:10
+ */
+public class TransferObjectPatternDemo {
+    public static void main(String[] args) {
+        StudentBO studentBusinessObject = new StudentBO();
+
+        //输出所有的学生
+        for (StudentVO student : studentBusinessObject.getAllStudents()) {
+            System.out.println("Student: [RollNo : "
+                    + student.getRollNo() + ", Name : " + student.getName() + " ]");
+        }
+
+        //更新学生
+        StudentVO student = studentBusinessObject.getAllStudents().get(0);
+        student.setName("Michael");
+        studentBusinessObject.updateStudent(student);
+
+        //获取学生
+        studentBusinessObject.getStudent(0);
+        System.out.println("Student: [RollNo : "
+                + student.getRollNo() + ", Name : " + student.getName() + " ]");
+    }
+}
